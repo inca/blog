862b8cf4b54f510252efa631e4fe6e2adb1ce95e
Boris Okunskiy <boris@okunskiy.name>
2020-04-27 15:23:53 +0300

    wip: leafgen v2

--- src/leafgen/leafgen.ts
+++ src/leafgen/leafgen.ts
@@ -6,32 +6,41 @@ export class LeafGenerator {
     width: number;
     height: number;
     ctx: CanvasRenderingContext2D;
-    imageData: ImageData;
+
+    maskMap: CanvasRenderingContext2D;
+
+    steps: number = 4;
+    seed: string = 'seed';
+    noiseScale: number = 20;
+
+    genes = [
+        { r: 0.9, a: 0 },
+        { r: 0.8, a: 40 },
+        { r: 0.8, a: -40 },
+        { r: 0.7, a: 95 },
+        { r: 0.7, a: -95 },
+        { r: 0.5, a: 120 },
+        { r: 0.5, a: -120 },
+    ];
 
     constructor(
         public canvas: HTMLCanvasElement,
-        public settings: {
-            seed: string,
-            noiseScale: number,
-            iterations: number,
-            initialNodes: Node[],
-        } = {
-                seed: 'seed',
-                noiseScale: 20,
-                iterations: 20,
-                initialNodes: [{ u: 0, v: 0, r: 1, parents: [] }],
-            },
     ) {
-        this.noise = new SimplexNoise(settings.seed);
+        this.noise = new SimplexNoise(this.seed);
         this.ctx = canvas.getContext('2d')!;
         this.width = canvas.width;
         this.height = canvas.height;
-        this.clear();
-        this.imageData = this.ctx.getImageData(0, 0, this.width, this.height);
+        clear(this.ctx);
+
+        const maskCanvas = document.createElement('canvas');
+        maskCanvas.width = this.width;
+        maskCanvas.height = this.height;
+        this.maskMap = maskCanvas.getContext('2d')!;
+        clear(this.maskMap);
     }
 
-    generate() {
-        const iter = this.drawSteps();
+    draw() {
+        const iter = this.traverse();
         const timer = setInterval(() => {
             const { done } = iter.next();
             if (done) {
@@ -42,95 +51,64 @@ export class LeafGenerator {
         }, 10);
     }
 
-    *drawSteps() {
-        for (const node of this.traverse()) {
-            for (const parent of node.parents) {
-                const [x1, y1] = this.uv2xy([parent.u, parent.v]);
-                const [x2, y2] = this.uv2xy([node.u, node.v]);
-                this.ctx.lineWidth = node.r * 20;
-                this.ctx.strokeStyle = 'rgba(255,255,255,.5)';
-                this.ctx.beginPath();
-                this.ctx.moveTo(x1 + 20, y1);
-                this.ctx.lineTo(x2 + 20, y2);
-                this.ctx.stroke();
-                yield;
-            }
-        }
-    }
-
     *traverse(): IterableIterator<Node> {
-        let queue: Node[] = this.settings.initialNodes;
-        const n = this.settings.iterations;
-        for (let i = 1; i <= n; i++) {
-            const t = i / n;
-            if (!queue.length) {
+        let frontier: Node[] = [
+            { step: 0, uv: [0.5, 0.5], a: 0, r: 1 },
+        ];
+
+        const stepSize = 0.1;   // Should be relative to number of steps, somehow.
+
+        for (let step = 1; step <= this.steps; step++) {
+            if (!frontier.length) {
                 return;
             }
-            let offsprings: Node[] = [];
-            for (const node of queue) {
-                const noise = this.sampleNoiseUV(node.u, node.v);
-                const childCount = chance(noise, 1, [.5, 2]);
-                const decay = Math.pow(1 - ((i - 1) / n), .25);
-                const maxR = node.r * Math.sqrt(childCount) * decay;
-                const radiuses = this.partition(maxR, childCount, noise);
-                for (const r of radiuses) {
-                    if (r < 0.025) {
+            const newFrontier: Node[] = [];
+            for (const parent of frontier) {
+                const offsprings: Node[] = this.genes.map(gene => {
+                    const r = gene.r * parent.r;
+                    const a = gene.a + parent.a;
+                    const uv1 = this.move(parent.uv, a, stepSize * r);
+                    const [x, y] = this.uv2xy(uv1);
+                    const existingPixel = getPixel(this.maskMap, x, y);
+                    const uv2 = this.move(parent.uv, a, stepSize * r * (1 - existingPixel[0]));
+                    return {
+                        parent,
+                        step,
+                        uv: uv2,
+                        r: r * (1 - existingPixel[0]),
+                        a
+                    };
+                });
+                for (const node of offsprings) {
+                    if (node.r < .01) {
+                        console.log('culled');
                         continue;
                     }
-                    offsprings.push({ u: 0, v: 0, r, parents: [node] });
+                    this.ctx.strokeStyle = 'rgba(255,255,255,.5)';
+                    this.ctx.lineWidth = 4 * node.r;
+                    this.ctx.beginPath();
+                    const [x1, y1] = this.uv2xy(parent.uv);
+                    const [x2, y2] = this.uv2xy(node.uv);
+                    this.ctx.moveTo(x1, y1);
+                    this.ctx.lineTo(x2, y2);
+                    this.ctx.stroke();
+
+                    // Draw to mask
+                    const grad = this.maskMap.createRadialGradient(x1, y1, 100 * parent.r, x2, y2, 10 * node.r);
+                    grad.addColorStop(0, 'rgba(255,0,0,0)');
+                    grad.addColorStop(1, 'rgba(255,0,0,.2)');
+                    this.maskMap.fillStyle = grad;
+                    this.maskMap.fillRect(0, 0, this.width, this.height);
+
+                    // Show mask
+                    this.ctx.putImageData(this.maskMap.getImageData(0, 0, this.width, this.height), 0, 0);
+
+                    yield node;
+                    newFrontier.push(node);
                 }
             }
-            // REDUCE
-            for (let c = 0; c < offsprings.length - 1; c++) {
-                const curr = offsprings[c];
-                const next = offsprings[c + 1];
-                if (next.r > curr.r * 2) {
-                    // Merge current into next
-                    next.r += curr.r / 2;
-                    next.parents.push(...curr.parents);
-                    offsprings.splice(c, 1);
-                    console.log('merged');
-                }
-            }
-            // Compute coords
-            let u = 0;
-            let v = t;
-            for (const node of offsprings) {
-                node.u = u;
-                node.v = v;
-                yield node;
-                // const noise = this.sampleNoiseUV(node.u, node.v) * 0.03;
-                u += 0.1 * Math.pow(node.r, .5);
-                v -= 0.05 * Math.pow(node.r, .5);
-            }
-            queue = offsprings;
+            frontier = newFrontier;
         }
-    }
-
-    draw() {
-        this.ctx.putImageData(this.imageData, 0, 0);
-    }
-
-    index(x: number, y: number) {
-        return (y * this.imageData.width + x) * 4;
-    }
-
-    getPixel(x: number, y: number): Vector4 {
-        const idx = this.index(x, y);
-        return [
-            this.imageData.data[idx + 0] / 255,
-            this.imageData.data[idx + 1] / 255,
-            this.imageData.data[idx + 2] / 255,
-            this.imageData.data[idx + 3] / 255,
-        ];
-    }
-
-    setPixel(x: number, y: number, v: number) {
-        const idx = this.index(x, y);
-        this.imageData.data[idx + 0] = v * 255;
-        this.imageData.data[idx + 1] = v * 255;
-        this.imageData.data[idx + 2] = v * 255;
-        this.imageData.data[idx + 3] = 255;
     }
 
     sampleNoiseXY(xy: Vector2) {
@@ -139,21 +117,16 @@ export class LeafGenerator {
     }
 
     sampleNoiseUV(u: number, v: number) {
-        const scale = this.settings.noiseScale;
+        const scale = this.noiseScale;
         return this.noise.noise2D(u * scale, v * scale) * .5 + .5;
     }
 
     drawNoise() {
         for (let y = 0; y < this.height; y++) {
             for (let x = 0; x < this.width; x++) {
-                this.setPixel(x, y, this.sampleNoiseXY([x, y]));
+                setPixel(this.ctx, x, y, this.sampleNoiseXY([x, y]));
             }
         }
-    }
-
-    clear() {
-        this.ctx.fillStyle = '#000';
-        this.ctx.fillRect(0, 0, this.width, this.height);
     }
 
     xy2uv(xy: Vector2): Vector2 {
@@ -164,6 +137,13 @@ export class LeafGenerator {
     uv2xy(uv: Vector2): Vector2 {
         const [u, v] = uv;
         return [u * this.width, (1 - v) * this.height];
+    }
+
+    move(uv: Vector2, angle: number, distance: number): Vector2 {
+        return [
+            uv[0] - Math.sin(angle * Math.PI / 180) * distance,
+            uv[1] + Math.cos(angle * Math.PI / 180) * distance,
+        ];
     }
 
     partition(value: number, count: number, power: number = 2) {
@@ -190,9 +170,38 @@ function chance<T>(rnd: number, from: T, ...thresholds: [number, T][]): T {
     return from;
 }
 
+function getPixel(ctx: CanvasRenderingContext2D, x: number, y: number): Vector4 {
+    const data = ctx.getImageData(x, y, 1, 1).data;
+    return [
+        data[0] / 255,
+        data[1] / 255,
+        data[2] / 255,
+        data[3] / 255,
+    ];
+}
+
+function setPixel(ctx: CanvasRenderingContext2D, x: number, y: number, v: number) {
+    const imgData = ctx.createImageData(1, 1);
+    imgData.data[0] = v * 255;
+    imgData.data[1] = v * 255;
+    imgData.data[2] = v * 255;
+    imgData.data[3] = 255;
+    ctx.putImageData(imgData, x, y);
+}
+
+function clear(ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
 export interface Node {
-    u: number;
-    v: number;
+    parent?: Node;
+    // Step
+    step: number;
+    // Coordinates
+    uv: Vector2;
+    // Angle
+    a: number,
+    // Radius
     r: number;
-    parents: Node[];
 }
